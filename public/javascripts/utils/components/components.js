a550f1b1d073effa2599dc3246bfec3ae3a26d42
clayton-scherer <cscherer93@aol.com>
2019-07-23 15:09:31 -0400

    EoD albums and artists render, AwA single view

--- public/javascripts/utils/components/components.js
+++ public/javascripts/utils/components/components.js
@@ -1,48 +1,48 @@
-import Html from '../Html/Html';
-import Api from '../api/Api';
+import Html from "../Html/Html";
+import Api from "../api/Api";
 export default () => new Components();
 
 class Components {
   getAppContext() {
-    return Html().select('#app');
+    return Html().select("#app");
   }
 
   getWrapperDiv() {
     return Html()
-      .create('div')
-      .addClass('wrapper');
+      .create("div")
+      .addClass("wrapper");
   }
 
   renderNavMenu() {
     const navMenu = Html()
-      .create('nav')
-      .addClass('nav-menu');
+      .create("nav")
+      .addClass("nav-menu");
 
     const navList = Html()
-      .create('ul')
-      .addClass('nav-menu__list');
+      .create("ul")
+      .addClass("nav-menu__list");
 
     const navListItemHome = Html()
-      .create('li')
-      .addClass('nav-menu__list-item')
-      .addAnchor('/#', 'Home')
+      .create("li")
+      .addClass("nav-menu__list-item")
+      .addAnchor("/#", "Home")
       .click(event => {
         event.preventDefault();
         this.renderPageHome();
       });
 
     const navListItemSongs = Html()
-      .create('li')
-      .addClass('nav-menu__list-item')
-      .addAnchor('/#', 'Songs')
+      .create("li")
+      .addClass("nav-menu__list-item")
+      .addAnchor("/#", "Songs")
       .click(event => {
         event.preventDefault();
         this.renderPageSongs();
       });
     const navListItemAlbums = Html()
-      .create('li')
-      .addClass('nav-menu__list-item')
-      .addAnchor('/#', 'Albums')
+      .create("li")
+      .addClass("nav-menu__list-item")
+      .addAnchor("/#", "Albums")
       .click(event => {
         event.preventDefault();
         this.renderPageAlbums();
@@ -56,12 +56,12 @@ class Components {
 
   renderMainHeader() {
     const mainHeader = Html()
-      .create('header')
-      .addClass('header');
+      .create("header")
+      .addClass("header");
     const mainHeaderTitle = Html()
-      .create('h1')
-      .addClass('header-title')
-      .text('Muzify');
+      .create("h1")
+      .addClass("header-title")
+      .text("Muzify");
     // const nav = this.renderMainNav();
     mainHeader.addChild(mainHeaderTitle);
     // mainHeader.addChild(nav);
@@ -70,71 +70,69 @@ class Components {
 
   renderMainFooter() {
     const mainFooter = Html()
-      .create('footer')
-      .addClass('footer');
+      .create("footer")
+      .addClass("footer");
     const mainFooterCopy = Html()
-      .create('small')
-      .addClass('copy')
-      .html('&copy; 2019 Muzify');
+      .create("small")
+      .addClass("copy")
+      .html("&copy; 2019 Muzify");
     mainFooter.addChild(mainFooterCopy);
     return mainFooter;
   }
 
   renderContentBlock(requestedData) {
     const contentBlock = Html()
-      .create('section')
-      .addClass('content-block');
+      .create("section")
+      .addClass("content-block");
 
     const contentTitle = Html()
-      .create('h2')
-      .addClass('content-title')
+      .create("h2")
+      .addClass("content-title")
       .text(requestedData);
 
     const contentList = Html()
-      .create('ul')
-      .addClass('content-list');
+      .create("ul")
+      .addClass("content-list");
 
     Api().getRequest(
       `http://localhost:3000/${requestedData}`,
       responseCollection => {
-        console.log(requestedData);
         responseCollection.forEach(item => {
           let name;
-          if (requestedData === 'artists') {
+          if (requestedData === "artists") {
             name = `${item.firstName} ${item.lastName} `;
           }
-          if (requestedData === 'songs') {
+          if (requestedData === "songs") {
             name = `${item.title}`;
           }
-          if (requestedData === 'albums') {
+          if (requestedData === "albums") {
             name = `${item.title}`;
           }
           const contentBlockListItem = Html()
-            .create('li')
-            .addClass('content-block__list-item')
+            .create("li")
+            .addClass("content-block__list-item")
             .addChild(
               Html()
-                .create('a')
-                .addAttribute('href', `/${requestedData}/${item.id}`)
+                .create("a")
+                .addAttribute("href", `${requestedData}/${item._id}`)
                 .text(name)
                 .click(event => {
-                  console.log(item);
                   event.preventDefault();
-                  const endpoint = event.target.getAttribute('href');
-                  const typeOfObject = endpoint.split('/')[1];
-                  if (typeOfObject === 'artists') {
-                    this.renderPageArtists(data);
-                  }
-                  if (typeOfObject === 'songs') {
-                    this.renderPageBooks(data);
-                  }
-                  if (typeOfObject === 'albums') {
-                    this.renderPageAlbums(data);
-                  }
 
+                  const endpoint = event.target.getAttribute("href");
                   Api().getRequest(
                     `http://localhost:3000/${endpoint}`,
                     data => {
+                      const typeOfObject = endpoint.split("/")[0];
+                      if (typeOfObject === "artists") {
+                        this.renderPageArtist(data);
+                      }
+                      if (typeOfObject === "songs") {
+                        this.renderPageSong(data);
+                      }
+                      if (typeOfObject === "albums") {
+                        this.renderPageAlbum(data);
+                      }
                       this.renderPageSingle(data, endpoint);
                     }
                   );
@@ -151,12 +149,12 @@ class Components {
 
   renderMainContent(requestedData) {
     const mainContent = Html()
-      .create('main')
-      .addClass('content');
+      .create("main")
+      .addClass("content");
 
     const containerDiv = Html()
-      .create('div')
-      .addClass('container');
+      .create("div")
+      .addClass("container");
 
     const contentBlock = this.renderContentBlock(requestedData);
     containerDiv.addChild(contentBlock);
@@ -164,40 +162,106 @@ class Components {
     return mainContent;
   }
 
-  renderPageArtists() {
-    const currentMainContentContainer = this.getWrapperDiv()
-      .select('.content')
-      .select('.container');
-    currentMainContentContainer.replace(this.renderContentBlock('artists'));
-    console.log('this is artist');
-  }
-  renderPageSongs() {
-    const currentMainContentContainer = this.getWrapperDiv()
-      .select('.content')
-      .select('.container');
-    currentMainContentContainer.replace(this.renderContentBlock('songs'));
-    console.log('this is songs');
-  }
-
   renderPageAlbums() {
     const currentMainContentContainer = this.getWrapperDiv()
-      .select('.content')
-      .select('.container');
-    currentMainContentContainer.replace(this.renderContentBlock('albums'));
-    console.log('this is albums');
+      .select(".content")
+      .select(".container");
+    currentMainContentContainer.replace(this.renderContentBlock("albums"));
   }
 
-  renderPageSingle() {
-    console.log(endpoint);
-    const typeOfObject = endpoint.split('/')[1];
-    console.log(typeOfObject);
-    if (typeOfObject === 'artists') {
+  renderPageArtists() {
+    const currentMainContentContainer = this.getWrapperDiv()
+      .select(".content")
+      .select(".container");
+    currentMainContentContainer.replace(this.renderContentBlock("artists"));
+  }
+
+  renderPageSongs() {
+    const currentMainContentContainer = this.getWrapperDiv()
+      .select(".content")
+      .select(".container");
+    currentMainContentContainer.replace(this.renderContentBlock("songs"));
+  }
+
+  renderPageAlbum(data) {
+    const currentMainContentContainer = this.getWrapperDiv()
+      .select(".content")
+      .select(".container")
+      .select(".content-block");
+
+    const albumTitle = Html()
+      .create("h3")
+      .addClass("content-title")
+      .text(data.title);
+
+    const albumSongs = Html()
+      .create("ul")
+      .addClass("album__song-list");
+    data.songList.forEach(song => {
+      const songElement = Html()
+        .create("li")
+        .addChild(
+          Html()
+            .create("a")
+            .addAttribute("href", `/songs/${song._id}`)
+            .text(song.title)
+        );
+      albumSongs.addChild(songElement);
+    });
+    currentMainContentContainer.replace(albumTitle);
+    currentMainContentContainer.addChild(albumSongs);
+  }
+
+  renderPageArtist(data) {
+    const currentMainContentContainer = this.getWrapperDiv()
+      .select(".content")
+      .select(".container")
+      .select(".content-block");
+
+    const artistName = Html()
+      .create("h3")
+      .addClass("content-title")
+      .text(data.firstName + " " + data.lastName);
+
+    const albums = Html()
+      .create("ul")
+      .addClass("album-list");
+    data.albumList.forEach(album => {
+      const albumElement = Html()
+        .create("li")
+        .addChild(
+          Html()
+            .create("a")
+            .addAttribute("href", `/albums/${album._id}`)
+            .text(album.title)
+        );
+      albums.addChild(albumElement);
+    });
+    currentMainContentContainer.replace(artistName);
+    currentMainContentContainer.addChild(albums);
+  }
+
+  renderPageSong(data) {
+    const currentMainContentContainer = this.getWrapperDiv()
+      .select(".content")
+      .select(".container")
+      .select(".content-block");
+    const albumTitle = Html()
+      .create("h3")
+      .addClass("content-title")
+      .text(data.title);
+    currentMainContentContainer.replace(albumTitle);
+  }
+
+  renderPageSingle(data, endpoint) {
+    const typeOfObject = endpoint.split("/")[1];
+    if (typeOfObject === "artists") {
       this.renderPageArtist(data);
     }
-    if (typeOfObject === 'songs') {
-      this.renderPageBook(data);
+    if (typeOfObject === "songs") {
+      this.renderPageSong(data);
     }
-    if (typeOfObject === 'albums') {
+    if (typeOfObject === "albums") {
       this.renderPageAlbum(data);
     }
   }
@@ -207,7 +271,7 @@ class Components {
     const wrapperDiv = this.getWrapperDiv();
     const mainHeader = this.renderMainHeader();
     const navMenu = this.renderNavMenu();
-    const mainContent = this.renderMainContent('artists');
+    const mainContent = this.renderMainContent("artists");
     const mainFooter = this.renderMainFooter();
     wrapperDiv.addChild(mainHeader);
     wrapperDiv.addChild(navMenu);
